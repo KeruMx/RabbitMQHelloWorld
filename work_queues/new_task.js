443bf58f4086ae702d94a11c4c432272a7d83fac
KeruMx <eemmanuel.lunaa@gmail.com>
2021-08-06 16:24:17 -0500

    WorkQueues

--- work_queues/new_task.js
+++ work_queues/new_task.js
@@ -1,10 +1,23 @@
-let queue = 'task_queue';
-let msg = process.argv.slice(2).join(' ') || "Hello World!";
+let amqp = require('amqplib/callback_api');
+amqp.connect('amqp://localhost', function (error, connection) {
+    if (error)
+        throw error;
+    connection.createChannel(function (eror1, channel) {
+        if (eror1)
+            throw error1;
+        let queue = 'task_queue';
+        let msg = process.argv.slice(2).join(' ') || "Hello World!";
 
-channel.assertQueue(queue, {
-    durable: true
-});
-channel.sendToQueue(queue, Buffer.from(msg), {
-    persistent: true
-});
-console.log(" [x] Sent '%s'", msg);
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.sendToQueue(queue, Buffer.from(msg), {
+            persistent: true
+        });
+        console.log(" [x] Sent '%s'", msg);
+    })
+    setTimeout(function () {
+        connection.close();
+        process.exit(0);
+    }, 500);
+})
